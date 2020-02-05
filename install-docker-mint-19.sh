33e93f0c8db369805bb39d1744e84adbab87cae4
Ruslan Ismailov <chifek@gmail.com>
2020-02-05 15:22:14 +0300

    Update install-docker-mint-19.sh

--- install-docker-mint-19.sh
+++ install-docker-mint-19.sh
@@ -1,1 +1,25 @@
-https://computingforgeeks.com/install-docker-and-docker-compose-on-linux-mint-19/
+#!/usr/bin/env bash
+
+sudo apt-get install -y \
+ apt-transport-https \
+ ca-certificates \
+ curl \
+ gnupg-agent \
+ software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(. /etc/os-release; echo "$UBUNTU_CODENAME") stable"
+cat /etc/apt/sources.list.d/additional-repositories.list
+
+sudo apt-get update
+sudo apt-get install docker-ce-cli -y
+sudo apt-get autoremove -y
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.24.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
